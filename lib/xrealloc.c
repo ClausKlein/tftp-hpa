c672c64fcd4c00d5c47af74ac67bcca07c9d8c8d
ClausKlein <claus.klein@arcormail.de>
2019-11-25 10:35:41 +0100

    added new files from ReadHead patches too

---
+++ lib/xrealloc.c
@@ -0,0 +1,20 @@
+/*
+ * xrealloc.c
+ *
+ * Simple error-checking version of realloc()
+ *
+ */
+
+#include "config.h"
+
+void *xrealloc(void *ptr, size_t size)
+{
+    void *p = realloc(ptr, size);
+
+    if (!p) {
+        fprintf(stderr, "Out of memory!\n");
+        exit(128);
+    }
+
+    return p;
+}
